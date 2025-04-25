ea2726d49f783de2b4228691237e5b84ab754c30
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-04-25 10:06:15 +0530

    Create perfectno.java

---
+++ perfectno.java
@@ -0,0 +1,29 @@
+//perfect number
+import java.util.*;
+public class program11{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+        int sum=0;
+        //find divisors
+        if(num==0) {
+            System.out.println("divisor is=0");
+        }
+
+            for (int i = 1; i < Math.abs(num); i++){
+            if(num%i==0){
+                System.out.println("divisor is="+i);
+                //sum it
+                sum+=i;
+            }}
+
+            System.out.println("sum is=" + sum);
+            if(sum==num){
+                System.out.println("perfect number");
+
+            }
+            else {
+                System.out.println("not");
+            }
+    }}
