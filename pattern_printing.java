fdbc1b04ad4fba1b58d85d7205ff929062f300e0
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-04-25 10:08:00 +0530

    Create pattern_printing.java

---
+++ pattern_printing.java
@@ -0,0 +1,37 @@
+//     *
+//     **
+//     ***
+//     ****
+//     *****
+//public class program13{
+//    public static void main(String[] args){
+//        int count=0;
+//        for(int i=1;i<=5;i++){
+//            for(int j=1;j<=i;j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+
+//*****
+//****
+//***
+//**
+//*
+public class program13{
+    public static void main(String[] args){
+        int count=0;
+        for(int i=5;i>=0;i--){
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
