801216f30738f8d893529f3fad55a622b04c8233
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-04-29 10:27:08 +0530

    Update perfectno.java

--- perfectno.java
+++ perfectno.java
@@ -7,10 +7,11 @@ public class program11{
         int num = sc.nextInt();
         int sum=0;
         //find divisors
-        if(num==0) {
-            System.out.println("divisor is=0");
+        if(num<=0) {
+            System.out.println("divisor is=0  and not a perfect number");
+            sc.close();
+            return;
         }
-
             for (int i = 1; i < Math.abs(num); i++){
             if(num%i==0){
                 System.out.println("divisor is="+i);
